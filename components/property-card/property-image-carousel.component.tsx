9a6740b4126ba07e8785e6d9e5dec7c59187631e
Adam Boucek <adamm01b@gmail.com>
2023-01-15 18:42:14 -0800

    feat: Add illustration images

---
+++ components/property-card/property-image-carousel.component.tsx
@@ -0,0 +1,21 @@
+// @flow
+import * as React from 'react';
+
+interface ImageData {
+  id: string;
+  imgUrl: string;
+  alt: string;
+}
+
+const IMAGE_DATA: ImageData[] = [
+  {
+    id: '1',
+    imgUrl: '',
+    alt: '',
+  },
+];
+
+type Props = {};
+export const PropertyImageCarousel = (props: Props) => {
+  return <div></div>;
+};
