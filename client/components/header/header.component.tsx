de200a835726fd1e88125f6350ed4a40955ecfac
Adam Boucek <adamm01b@gmail.com>
2023-02-09 21:23:27 -0800

    feat: Extend header component

--- client/components/header/header.component.tsx
+++ client/components/header/header.component.tsx
@@ -3,42 +3,49 @@ import Image from 'next/image';
 import Link from 'next/link';
 import * as React from 'react';
 import { Button } from '../button/button.component';
-type Props = {};
-export const Header = (props: Props) => {
+type Props = {
+  position: 'static' | 'relative' | 'absolute' | 'sticky' | 'fixed';
+  imageExtend: boolean;
+  type: 'transparent' | 'solid';
+};
+export const Header = ({ position, imageExtend }: Props) => {
   return (
-    <header className='header '>
-      <div className='row container'>
-        <Link href='/'>
-          <Image src='/static/logos/ahoy-logo-white.png' height='64' width='315' alt='Ahoy House logo' />
-        </Link>
-        <ul className='menu__links'>
-          <li>
-            <Link className='btn--link' href='/'>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link className='btn--link' href='/'>
-              Properties
-            </Link>
-          </li>
-          <li>
-            <Link className='btn--link' href='/'>
-              About
-            </Link>
-          </li>
-        </ul>
-        <ul className='menu__login'>
-          <li>
-            <Link className='btn--link' href='/'>
-              Login
-            </Link>
-          </li>
-          <li>
-            <Button className='secondary'>Sign Up</Button>
-          </li>
-        </ul>
-      </div>
-    </header>
+    <>
+      <header className='header' style={{ position: position }}>
+        <nav className='row container'>
+          <Link href='/'>
+            <Image src='/static/logos/ahoy-logo-white.png' height='64' width='315' alt='Ahoy House logo' />
+          </Link>
+          <ul className='menu__links'>
+            <li>
+              <Link className='btn--link' href='/'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className='btn--link' href='/'>
+                Properties
+              </Link>
+            </li>
+            <li>
+              <Link className='btn--link' href='/'>
+                About
+              </Link>
+            </li>
+          </ul>
+          <ul className='menu__login'>
+            <li>
+              <Link className='btn--link' href='/'>
+                Login
+              </Link>
+            </li>
+            <li>
+              <Button className='secondary'>Sign Up</Button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      {imageExtend ? <div className='header__image'></div> : null}
+    </>
   );
 };
