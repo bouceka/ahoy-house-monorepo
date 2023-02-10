e55ca6f5d44f144ac21eaf3172818a58218b29fd
Adam Boucek <adamm01b@gmail.com>
2023-02-09 21:23:40 -0800

    feat: Add hero WIP

---
+++ client/components/hero/hero.component.tsx
@@ -0,0 +1,12 @@
+// @flow
+import * as React from 'react';
+type Props = {};
+export const Hero = (props: Props) => {
+  return (
+    <section className='hero'>
+      <div className='abs-center'>
+        <h2>asdasdas</h2>
+      </div>
+    </section>
+  );
+};
