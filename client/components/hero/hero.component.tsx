eebad13d41cf357716759a0d5de5dafb6fcb88d7
bouceka <adamm01b@gmail.com>
2023-02-11 21:58:55 -0800

    feat: Add MVP hero

--- client/components/hero/hero.component.tsx
+++ client/components/hero/hero.component.tsx
@@ -1,11 +1,13 @@
 // @flow
 import * as React from 'react';
+import { Action } from '../action/action.component';
 type Props = {};
 export const Hero = (props: Props) => {
   return (
     <section className='hero'>
-      <div className='abs-center'>
-        <h2>asdasdas</h2>
+      <div className='hero__container'>
+        <h2 className='display--large'>Make yourself at home You are always wanted</h2>
+        <Action as='link' href='/properties' styleType='primary'>Show properties</Action>
       </div>
     </section>
   );
