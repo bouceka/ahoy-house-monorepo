1453233d98357adbc9b373908b067e5589b4da4c
bouceka <adamm01b@gmail.com>
2023-02-14 22:03:50 -0800

    feat: Add input component

---
+++ client/components/input/input.component.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  helpText?: string;
+  type?: string;
+}
+export const Input = ({ label, type = 'text', helpText, ...otherProps }: Props) => {
+  return (
+    <div className='form-item'>
+      {label ? <label htmlFor={otherProps.name}>{label}</label> : null}
+      <input type={type} className='form-input' {...otherProps} />
+      {helpText ? <span className='help-text'>{helpText}</span> : null}
+    </div>
+  );
+};
