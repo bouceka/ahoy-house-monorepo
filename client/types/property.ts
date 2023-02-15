0ea05a61ff04a4745ebdc9e09cda0fea994d4929
bouceka <adamm01b@gmail.com>
2023-02-14 22:10:33 -0800

    feat: Add property card styling + TS types

---
+++ client/types/property.ts
@@ -0,0 +1,29 @@
+export interface Property {
+  id: string;
+  title: string;
+  numberRooms: number;
+  postCode: string;
+  numberBaths: number;
+  size: number;
+  name: string;
+  description: string;
+  address: string;
+  images: PropertyImage[];
+	rooms:Room[]
+}
+
+export interface PropertyImage {
+	id:string,
+	url:string,
+	alt:string
+}
+
+
+export interface Room {
+	id:string,
+	name:string,
+	capacity:number,
+	description:string,
+	size:number,
+	pricePerNight: number,
+}
