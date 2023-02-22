dd1f894660d035024daf092d0befe0859a1a86dc
Adam Boucek <adamm01b@gmail.com>
2023-02-22 14:41:03 -0800

    feat: Add config for displaying SVG and cloudinary images

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+  images: {
+    domains: ['res.cloudinary.com']
+  }
 }
 
 module.exports = nextConfig
