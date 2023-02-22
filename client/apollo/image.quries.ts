973ccc0ee7008c64320f8981ef9a81423c5a72f0
Adam Boucek <adamm01b@gmail.com>
2023-02-22 15:09:09 -0800

    feat: Add image and room queries

---
+++ client/apollo/image.quries.ts
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+
+export const UPLOAD_IMAGE = gql`
+  mutation uploadImage($file: Upload!, $propertyId: String!) {
+    uploadImage(
+      file: $file
+      createImageInput: {
+        propertyId: $propertyId
+      }
+    ) {
+      id
+    }
+  }
+`;
+
+export const DELETE_IMAGE = gql`
+mutation deleteImage(
+    $id: String!
+  ) {
+    deleteImage(
+      id: $id
+    ) {
+      url
+    }
+  }`
+
---
+++ client/apollo/room-queries.ts
@@ -0,0 +1,94 @@
+import { gql } from '@apollo/client';
+
+export const DELETE_ROOM = gql`
+  mutation deleteRoom($id: String!) {
+    deleteRoom(id: $id) {
+      name
+    }
+  }
+`;
+
+export const UPDATE_ROOM = gql`
+  mutation updateRoom(
+    $id: ID!
+    $capacity: Float!
+    $name: String!
+    $description: String!
+    $livingArea: Float!
+    $propertyId: ID!
+    $amenities: String!
+    $pricePerNight: Float!
+  ) {
+    updateRoom(
+      updateRoomInput: {
+        description: $description
+        capacity: $capacity
+        name: $name
+        amenities: $amenities
+        livingArea: $livingArea
+        propertyId: $propertyId
+        pricePerNight: $pricePerNight
+        id: $id
+      }
+    ) {
+      name
+    }
+  }
+`;
+
+export const GET_ALL_ROOMS = gql`
+  query getAllRooms {
+    getAllRooms {
+      id
+      description
+      livingArea
+      capacity
+      name
+      propertyId
+      pricePerNight
+      amenities
+    }
+  }
+`;
+
+export const GET_ROOM = gql`
+  query getRoom($id: ID!) {
+    getRoom(id: $id) {
+      id
+      description
+      livingArea
+      capacity
+      propertyId
+      name
+      pricePerNight
+      amenities
+    }
+  }
+`;
+
+
+export const CREATE_ROOM = gql`
+  mutation createRoom(
+    $capacity: Float!
+    $name: String!
+    $description: String!
+    $livingArea: Float!
+    $propertyId: ID!
+    $amenities: String!
+    $pricePerNight: Float!
+  ) {
+    createRoom(
+      createRoomInput: {
+        description: $description
+        capacity: $capacity
+        name: $name
+        amenities: $amenities
+        livingArea: $livingArea
+        propertyId: $propertyId
+        pricePerNight: $pricePerNight
+      }
+    ) {
+      name
+    }
+  }
+`;
