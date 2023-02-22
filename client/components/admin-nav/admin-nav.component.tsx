b664bc0dd295030ede2b069c2c8cd0642d067c39
Adam Boucek <adamm01b@gmail.com>
2023-02-22 14:26:21 -0800

    feat: Craete admin navbar

--- client/components/admin-nav/admin-nav.component.tsx
+++ client/components/admin-nav/admin-nav.component.tsx
@@ -1,38 +1,31 @@
-// @flow 
+// @flow
 import Link from 'next/link';
 import Image from 'next/image';
 import * as React from 'react';
 import { Action } from '../action/action.component';
-type Props = {
-    
-};
+type Props = {};
 export const AdminNav = (props: Props) => {
-    return (
-        <>
-        <header className='header' style={{ position: 'fixed', top:0 }}>
-          <nav className='row container'>
-            <Link href='/'>
-              <Image src='/static/logos/ahoy-logo-white.png' height='64' width='315' alt='Ahoy House logo' />
-            </Link>
-            <ul className='menu__links'>
-              <li>
-                <Link className='btn--link' href='/admin/add-property'>
-                  Add property
-                </Link>
-              </li>
-              <li>
-                <Link className='btn--link' href='/'>
-                  Property List
-                </Link>
-              </li>
-              <li>
-                <Link className='btn--link' href='/'>
-                  Order History
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-      </>
-    );
+  return (
+    <>
+      <header className='header' style={{ position: 'fixed', top: 0 }}>
+        <nav className='row container'>
+          <h2 className='heading' style={{ color: '#fff' }}>
+            Admin
+          </h2>
+          <ul className='menu__links'>
+            <li>
+              <Link className='btn--link' href='/'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className='btn--link' href='/admin/properties'>
+                Properties
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 };
