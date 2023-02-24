b2093b69c650ffeca9f84ebbba9d97dc2abda598
Adam Boucek <adamm01b@gmail.com>
2023-02-23 20:44:54 -0800

    feat: Make responsive header

--- client/components/header/header.component.tsx
+++ client/components/header/header.component.tsx
@@ -3,51 +3,65 @@ import Image from 'next/image';
 import Link from 'next/link';
 import * as React from 'react';
 import { Action } from '../action/action.component';
+import useWindowDimensions from '../../hooks/window-dimension';
 type Props = {
   position: 'static' | 'relative' | 'absolute' | 'sticky' | 'fixed';
   imageExtend: boolean;
   type: 'transparent' | 'solid';
 };
 export const Header = ({ position, imageExtend = false, type = 'solid' }: Props) => {
+  const { width, height } = useWindowDimensions();
   return (
     <>
       <header className='header' style={{ position: position }}>
-        <nav className='row container'>
-          <Link href='/'>
-            <Image src='/static/logos/ahoy-logo-white.png' height='64' width='315' alt='Ahoy House logo' />
-          </Link>
-          <ul className='menu__links'>
-            <li>
+        <div className='row'>
+          <nav className=' container'>
+            <Link href='/'>
+              {width && width >= 600 ? (
+                <Image src='/static/logos/ahoy-logo-white.png' height='64' width='315' alt='Ahoy House logo' />
+              ) : (
+                <Image src='/static/logos/ahoy-logo-small.png' height='64' width='64' alt='Ahoy House logo' />
+              )}
+            </Link>
+            <input className='menu__btn' type='checkbox' id='menu-btn' />
+            <label className='menu__icon' htmlFor='menu-btn'>
+              <span className='icon'></span>
+            </label>
+
+            <ul className='menu__links'>
+              <li>
+                <Link className='btn--link' href='/'>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className='btn--link' href='/properties'>
+                  Properties
+                </Link>
+              </li>
+              <li>
+                <Action as='button' styleType='secondary'>
+                  Sign Up
+                </Action>
+              </li>
+              {/* <li>
               <Link className='btn--link' href='/'>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className='btn--link' href='/properties'>
-                Properties
-              </Link>
-            </li>
-            {/* <li>
-              <Link className='btn--link' href='/'>
-                About
+              About
               </Link>
             </li> */}
-          </ul>
-          <ul className='menu__login'>
+            </ul>
+            {/* <ul className='menu__login'>
             <li>
-              <Link className='btn--link' href='/login'>
-                Login
-              </Link>
+            <Link className='btn--link' href='/login'>
+            Login
+            </Link>
             </li>
-            <li>
-              <Action as='button' styleType='secondary'>
-                Sign Up
-              </Action>
-            </li>
-          </ul>
-        </nav>
+            
+          </ul> */}
+          </nav>
+        </div>
+        {imageExtend ? <div className='header__image'></div> : null}
       </header>
-      {imageExtend ? <div className='header__image'></div> : null}
     </>
   );
 };
---
+++ client/hooks/window-dimension.ts
@@ -0,0 +1,29 @@
+
+import { useEffect, useState } from 'react';
+// from https://dev.to/adrien/creating-a-custom-react-hook-to-get-the-window-s-dimensions-in-next-js-135k
+type WindowDimentions = {
+    width: number | undefined;
+    height: number | undefined;
+};
+
+const useWindowDimensions = (): WindowDimentions => {
+    const [windowDimensions, setWindowDimensions] = useState<WindowDimentions>({
+        width: undefined,
+        height: undefined,
+    });
+    useEffect(() => {
+        function handleResize(): void {
+            setWindowDimensions({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return (): void => window.removeEventListener('resize', handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+
+    return windowDimensions;
+};
+
+export default useWindowDimensions;
