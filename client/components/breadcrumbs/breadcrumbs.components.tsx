5aa98ebf33c36cd9c1690651ea95dbbaf3a59311
Adam Boucek <adamm01b@gmail.com>
2023-03-25 14:02:03 -0700

    feat: Add breadcrumbs

---
+++ client/components/breadcrumbs/breadcrumbs.components.tsx
@@ -0,0 +1,43 @@
+// @flow
+import * as React from 'react';
+import { Action } from '../action/action.component';
+import { useRouter } from 'next/router';
+
+const capitalize = (word: string) => word.charAt(0).toUpperCase() + word.slice(1);
+
+const styleCrumb = (crumb: string) => {
+  return capitalize(crumb.replace('-', ' '));
+};
+export const Breadcrumbs = () => {
+  const router = useRouter();
+
+  let currentLink = '';
+
+  const crumbs = router.pathname
+    .split('/')
+    .filter((crumb) => crumb !== '')
+    .map((crumb) => {
+      currentLink += `/${crumb}`;
+      if (crumb !== '[slug]')
+        return (
+          <div className='crumb' key={crumb}>
+            <Action as='link' styleType='link' href={currentLink}>
+              {styleCrumb(crumb)}
+            </Action>
+          </div>
+        );
+    });
+
+  crumbs.unshift(
+    <div className='crumb' key='home'>
+      <Action as='link' styleType='link' href={'/'}>
+        Home
+      </Action>
+    </div>
+  );
+  return (
+    <div>
+      <div className='row breadcrumbs'>{crumbs}</div>
+    </div>
+  );
+};
